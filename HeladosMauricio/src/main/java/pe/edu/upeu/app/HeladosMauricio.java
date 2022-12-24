ef8e3974ce9f517c261697bffe8effe0849d2398
henrrylidhuardohanccosucari <109683031+henrrylidhuardohanccosucari@users.noreply.github.com>
2022-12-24 10:47:19 -0500

    Add files via upload

---
+++ HeladosMauricio/src/main/java/pe/edu/upeu/app/HeladosMauricio.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package pe.edu.upeu.app;
+
+import pe.edu.upeu.app.gui.Login;
+
+
+/**
+ *
+ * @author DELL
+ */
+public class HeladosMauricio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        new Login().setVisible(true);
+        //new ReporteVentas().setVisible(true);
+    }
+}
---
+++ HeladosMauricio/src/main/java/pe/edu/upeu/app/util/UtilsX.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pe.edu.upeu.app.util;
+
+import java.net.URL;
+
+/**
+ *
+ * @author DELL
+ */
+public class UtilsX {
+
+    public URL getFile(String ruta) {
+        return this.getClass().getResource("/" + ruta);
+    }
+
+}
+
